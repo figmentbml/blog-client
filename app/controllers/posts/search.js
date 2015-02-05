eedb3232c3bffd09a40bc0a490996172f7056602
figmentbml <beth.jaswa@gmail.com>
2015-02-05 16:23:11 -0700

    search bar works great

--- app/controllers/posts/search.js
+++ app/controllers/posts/search.js
@@ -3,6 +3,10 @@ import Ember from "ember";
 export default Ember.ArrayController.extend({
 
   queryParams: ['term'],
-  term: null
+  term: null,
+
+  results: function () {
+    return this.store.find('post', {find: this.get('term')});
+  }.property('term')
 
 });
--- app/routes/posts/search.js
+++ app/routes/posts/search.js
@@ -2,12 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  model: function(params) {
-    if(params.term) {
-      return this.store.find('post', {find: params.term});
-    } else {
-      return [];
-    }
-  }
-
 });
