514083bef67bfc85b5f049130194366ed48351f1
figmentbml <beth.jaswa@gmail.com>
2015-02-18 14:11:27 -0700

    users can select a user when creating a new post

--- app/controllers/posts/new.js
+++ app/controllers/posts/new.js
@@ -2,29 +2,39 @@ import Ember from "ember";
 
 export default Ember.ArrayController.extend({
 
+  currentUser: {
+    id: 1
+  },
+
   actions: {
     newPost: function(){
+      var controller = this;
       var title = this.get('titleCopy');
       var body = this.get('bodyCopy');
 
       if ((title == null) || (body == null)) {
-        this.flashMessage({
+        controller.flashMessage({
           content: 'You cannot have an empty title or body.',
           duration: 1000,
           type: 'fail',
         });
       } else {
-        var post = this.store.createRecord('post', { title: title, body: body });
-        this.set('titleCopy', '');
-        this.set('bodyCopy', '');
-        post.save().then(function(){
-          this.transitionToRoute('posts');
-          this.flashMessage({
-            content: 'Your post was saved!',
-            duration: 1000,
-            type: 'success',
+        controller.store.find('user', this.get('userCopy')).then(function(user){
+          var post = controller.store.createRecord('post', { title: title, body: body, user: user });
+          controller.set('titleCopy', '');
+          controller.set('bodyCopy', '');
+          post.save().then(function(){
+            controller.transitionToRoute('posts');
+            controller.flashMessage({
+              content: 'Your post was saved!',
+              duration: 1000,
+              type: 'success',
+            });
           });
-        }.bind(this));
+
+        });
+
+
       }
       }
 
